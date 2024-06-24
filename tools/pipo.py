f11a158a3998a798986476992e2faa5da64cb8f5
garzol <dubophil@free.fr>
2024-06-24 18:17:24 +0200

    test2.coe created

--- tools/pipo.py
+++ tools/pipo.py
@@ -3,7 +3,10 @@
 import sys
 
 for i in range(0,256):    
-    s1 = i&0x0F
+    if i%16==0:
+        s1=i//16
+    else:
+        s1 = i&0x0F
         
     s = s1.to_bytes(1, "big")
     sys.stdout.buffer.write(s)
