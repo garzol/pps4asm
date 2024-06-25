f0f0b4bfe4c764bed8491e1a3d1c2b624d796670
garzol <dubophil@free.fr>
2024-06-25 09:51:51 +0200

    added aaa 555 coe files

--- tools/pipo.py
+++ tools/pipo.py
@@ -2,13 +2,43 @@
 
 import sys
 
-for i in range(16):
-    for j in range(16):
+
+def pattern4():
+    for i in range(16):
+        for j in range(16):
+            
+            if j==0 or j==1:
+                s1=i
+            else:
+                s1 = ((i+j)%16)//2
+            
+            s = s1.to_bytes(1, "big")
+            sys.stdout.buffer.write(s)
+
+
+def pattern555():
+    s5 = 5
+    for i in range(16):
+        for j in range(16):
+            s = s5.to_bytes(1, "big")
+            sys.stdout.buffer.write(s)
+
+def patternAAA():
+    sx = 10
+    for i in range(16):
+        for j in range(16):
+            s = sx.to_bytes(1, "big")
+            sys.stdout.buffer.write(s)
+
+
+if __name__ == "__main__":
+    # try:
+    #     fn = sys.argv[1]
+    # except:
+    #     print("Bin file argument required ...Abort.")
         
-        if j==0 or j==1:
-            s1=i
-        else:
-            s1 = ((i+j)%16)//2
-        
-        s = s1.to_bytes(1, "big")
-        sys.stdout.buffer.write(s)
+    patternAAA()
+
+            
+            
+            
